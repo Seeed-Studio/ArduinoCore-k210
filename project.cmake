aec9a5d9120caec894fe749c06970a01cb932b51
Baozhu Zuo <baozhu.zuo@gmail.com>
2019-03-19 17:34:36 +0800

    add missing file headers

--- project.cmake
+++ project.cmake
@@ -5,7 +5,9 @@ SET_PROPERTY(SOURCE ${PROJ_LIB_SRC_INO} PROPERTY LANGUAGE CXX)
 SET_SOURCE_FILES_PROPERTIES(${PROJ_LIB_SRC_INO} PROPERTIES COMPILE_FLAGS "-x c++")
 
 include_directories(${CMAKE_CURRENT_LIST_DIR}/variants/k210
-                    ${CMAKE_CURRENT_LIST_DIR}/cores/k210)
+                    ${CMAKE_CURRENT_LIST_DIR}/cores/k210
+                    ${SDK_ROOT}/third_party
+                    ${SDK_ROOT}/lib/hal/include)
             
 target_sources(${PROJECT_NAME} PUBLIC ${PROJ_LIB_SRC_INO})
 
