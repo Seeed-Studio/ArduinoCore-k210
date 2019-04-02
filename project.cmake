2bb941fee14e515bd7ad454c99056b81ca1f3ec9
Baozhu Zuo <baozhu.zuo@gmail.com>
2019-04-02 19:48:33 +0800

    define ARDUINO=1000, add i2c related libraries

--- project.cmake
+++ project.cmake
@@ -1,11 +1,16 @@
 FILE(GLOB_RECURSE PROJ_LIB_SRC_INO
-    "${CMAKE_CURRENT_LIST_DIR}/*.ino"
+    "${CMAKE_CURRENT_LIST_DIR}/libraries/built-in/*.ino"
     )
 SET_PROPERTY(SOURCE ${PROJ_LIB_SRC_INO} PROPERTY LANGUAGE CXX)
 SET_SOURCE_FILES_PROPERTIES(${PROJ_LIB_SRC_INO} PROPERTIES COMPILE_FLAGS "-x c++")
 
+add_definitions(-DARDUINO=1000 )
+
 include_directories(${CMAKE_CURRENT_LIST_DIR}/variants/k210
                     ${CMAKE_CURRENT_LIST_DIR}/cores/k210
+                    ${CMAKE_CURRENT_LIST_DIR}/libraries/Wire
+                    ${CMAKE_CURRENT_LIST_DIR}/libraries/Accelerometer_ADXL345
+                    ${CMAKE_CURRENT_LIST_DIR}/libraries/ADS1115
                     ${SDK_ROOT}/third_party
                     ${SDK_ROOT}/lib/hal/include)
             
