913b18c67cefa9c645f2df35dad4166152599bfa
Baozhu Zuo <baozhu.zuo@gmail.com>
2019-04-07 05:56:25 +0000

    add gpiohs interrupts api

--- cores/k210/WInterrupts.h
+++ cores/k210/WInterrupts.h
@@ -23,30 +23,42 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#include "pins_arduino.h"
-#include "gpiohs.h"
-#include "WInterrupts.h"
 
-int gpiohs_irq_callback_t(void *ctx){
-    voidFuncPtr func = ctx;
-    func();
-    return 0;
-}
+#ifndef _WIRING_INTERRUPTS_
+#define _WIRING_INTERRUPTS_
 
- /*
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//      LOW 0
+//      HIGH 1
+#define CHANGE 3
+#define FALLING 1
+#define RISING 2
+
+#define DEFAULT 1
+#define EXTERNAL 4
+
+
+
+typedef void (*voidFuncPtr)(void);
+
+/*
  * \brief Specifies a named Interrupt Service Routine (ISR) to call when an interrupt occurs.
  *        Replaces any previous function that was attached to the interrupt.
  */
-void attachInterrupt(uint32_t pin, voidFuncPtr callback, uint32_t mode){
-    fpioa_set_function(pin, pin_map[pin]);
-    gpiohs_set_drive_mode(pin, GPIO_DM_INPUT_PULL_UP);
-    gpiohs_set_pin_edge(pin, GPIO_PE_BOTH);
+void attachInterrupt(uint32_t pin, voidFuncPtr callback, uint32_t mode);
 
-    gpiohs_irq_register(pin, 1, gpiohs_irq_callback_t, callback);
-}
 /*
  * \brief Turns off the given interrupt.
  */
-void detachInterrupt(uint32_t pin){
-    gpiohs_irq_unregister(pin);
+void detachInterrupt(uint32_t pin);
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif
